9d2f1fa5edac29c2b4c226703e7535306a395663
Bodhi <h.bahadirggbkn@gmail.com>
2023-04-08 13:02:18 +0200

    Contract and dates

--- src/EverythingEverywhereAllAtOnce/Contract.java
+++ src/EverythingEverywhereAllAtOnce/Contract.java
@@ -2,19 +2,48 @@ package EverythingEverywhereAllAtOnce;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Contract {
+    static Scanner scan = new Scanner(System.in);
     private LocalDate rent_Start;
     private LocalDate rent_end;
     private LocalTime rentstarttime;
     private LocalTime rentendtime;
 
     public Contract(LocalDate rent_Start, LocalDate rent_end, LocalTime rentstarttime, LocalTime rentendtime) {
-        this.rent_Start = rent_Start;
-        this.rent_end = rent_end;
+        setRent_Start(rent_Start);
+        setRent_end(rent_end);
         this.rentstarttime = rentstarttime;
         this.rentendtime = rentendtime;
     }
 
+    private void setRent_Start(LocalDate rent_Start) {
+        if (LocalDate.now().isAfter(rent_Start)) {
+            this.rent_Start = rent_Start;
+            return ;
+        }
+        System.out.println("wrong date its old enter a new date");
+        try {
+            setRent_Start(LocalDate.of(scan.nextInt(), scan.nextInt(), scan.nextInt()));
+        }catch (Exception e){
+            System.out.println("first year month and day");
+            setRent_Start(LocalDate.of(2012,5,3));
+        }
+    }
+
+    public void setRent_end(LocalDate rent_end) {
+        if (rent_Start.isAfter(rent_end)) {
+            this.rent_end = rent_end;
+            return;
+        }
+        System.out.println("wrong date its old enter a new date");
+        try {
+            setRent_end(LocalDate.of(scan.nextInt(), scan.nextInt(), scan.nextInt()));
+        }catch (Exception e){
+            System.out.println("first year month and day");
+            setRent_end(LocalDate.of(2012,5,3));
+        }
+    }
 
 }
--- src/EverythingEverywhereAllAtOnce/Runner.java
+++ src/EverythingEverywhereAllAtOnce/Runner.java
@@ -1,9 +1,11 @@
 package EverythingEverywhereAllAtOnce;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Runner {
     public static void main(String[] args) {
-        Map<String ,Map<String,Object>> carList =
+        Map<String ,Map<String,Object>> carList = new HashMap<>();
+
     }
 }
